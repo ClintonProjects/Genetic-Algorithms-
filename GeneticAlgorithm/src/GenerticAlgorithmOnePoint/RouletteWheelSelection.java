dddec71a0bb8a9f4a6df0a8c964183a1d993bde6
TengWang001 <957219706@qq.com>
2021-12-12 22:54:43 +0000

    select

--- GeneticAlgorithm/src/GenerticAlgorithmOnePoint/RouletteWheelSelection.java
+++ GeneticAlgorithm/src/GenerticAlgorithmOnePoint/RouletteWheelSelection.java
@@ -1,20 +1,79 @@
 package GenerticAlgorithmOnePoint;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 import GeneticAlgorithm.Couple;
+import GeneticAlgorithm.FitnessEvaluator;
 import GeneticAlgorithm.Individual;
 import GeneticAlgorithm.Population;
 import StrategyPattern.Selection;
+
 
 public class RouletteWheelSelection implements  Selection {
 
 
 	@Override
+	
+	
 	public Couple select(Population population) {
-		// TODO Auto-generated method stub
-		return null;
+		Couple newCouple= new Couple();
+		newCouple.Individual1=this.selectIndvidual(population).deepCopy();
+		
+		newCouple.Individual2=this.selectIndvidual(population).deepCopy();
+		
+		return newCouple;
+		
 	}
+	public Individual selectIndvidual(Population population) {
+		// TODO Auto-generated method stub
+		FitnessEvaluator fitnessEvaluator=new FitnessEvaluator();
+		fitnessEvaluator.updateFitness(population);
+		
+		Individual talentSpecies=null;
+		
+		Individual SelectedIndiv=null;
+		
+		ListIterator<Individual> P_iterator =population.getPopulation().listIterator();
+		float talentDis=Float.MAX_VALUE;
+		float totalDis=0;
+		while(P_iterator.hasNext())
+		{
+			if(talentDis > P_iterator.next().getFitness())
+			{
+				talentDis=P_iterator.next().getFitness();
+				talentSpecies=P_iterator.next().deepCopy();
+			}
+		}
 
+		Population newPopulation=new Population();
+		int talentNum=(int)(population.getPopulation().size()/5);
+		for(int i=1;i<=talentNum;i++)
+		{
+			Individual newSpecies=talentSpecies.deepCopy();
+			population.getPopulation().add(newSpecies);
+	
+		}
+		fitnessEvaluator.updateFitness(population);
+		P_iterator =population.getPopulation().listIterator();
+		while(P_iterator.hasNext()) {
+		totalDis+=P_iterator.next().getFitness();
+		}
+		
+		float r = (float) (Math.random()*totalDis);
+		
+		P_iterator =population.getPopulation().listIterator();
+		while(P_iterator.hasNext()&&P_iterator.next()!=talentSpecies){
+			float sum = 0;
+			sum+=P_iterator.next().getFitness();
+			if(sum>r) {
+				SelectedIndiv=P_iterator.previous().deepCopy();
+				break;
+			}			
+		}
+		
+		return SelectedIndiv;
+		
+	}
 
 }
