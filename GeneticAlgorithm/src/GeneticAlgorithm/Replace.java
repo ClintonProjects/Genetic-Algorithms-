cc9705841ba38d740c353e2ba7f0a2bfd6a8f9da
TengWang001 <957219706@qq.com>
2021-12-12 23:09:21 +0000

    aa

--- GeneticAlgorithm/src/GeneticAlgorithm/Replace.java
+++ GeneticAlgorithm/src/GeneticAlgorithm/Replace.java
@@ -1,5 +1,13 @@
 package GeneticAlgorithm;
+import StrategyPattern.*;
 
-public class Replace {
+
+public class Replace implements Replacer{
+
+	@Override
+	public void relace(Population p, Couple couple) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
