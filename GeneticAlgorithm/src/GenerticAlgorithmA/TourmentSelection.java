65ba38bad1e5ac3bb663fe3caf1ed09c9b926ff9
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2021-12-13 02:01:06 +0000

    Couple update

--- GeneticAlgorithm/src/GenerticAlgorithmA/TourmentSelection.java
+++ GeneticAlgorithm/src/GenerticAlgorithmA/TourmentSelection.java
@@ -84,6 +84,34 @@ public class TourmentSelection implements Selection {
 		return newList;
 	}
 
+	@Override
+	public Couple select(Population population) {
+		ArrayList<Individual> newList = population.getPopulation();
+
+		// This code will be change from to the actual Algorithm, it just temp
+		Random rand = new Random();
+
+		// making sure the population count is an even number, this will cause issue
+		// since it going to remove 1 item
+		int selectionReturnSize = newList.size() / 2;
+		if (selectionReturnSize % 2 == 1) selectionReturnSize = -1;
+
+			// sets two random members to indivuals
+			Individual indAToBeReturned = newList.get(rand.nextInt(newList.size()));
+			Individual indBToBeReturned = newList.get(rand.nextInt(newList.size()));
+
+			// checks if are two Individuals are same, if they are gets a new one
+			if (indAToBeReturned == indBToBeReturned) do
+				indBToBeReturned = newList.get(rand.nextInt(newList.size()));
+			while (indAToBeReturned == indBToBeReturned);
+
+			// removes the Individuals from the list since they can no longer be used
+			newList.remove(indAToBeReturned);
+			newList.remove(indBToBeReturned);
+
+		return new Couple(indAToBeReturned, indBToBeReturned);
+	}
+
 
 
 }
